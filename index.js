a2437e310c5280bfc5aac70c552b625ee4f1c70d
mnnlthmpsn <mnnlthmpsn@outlook.com>
2021-02-11 02:48:56 +0000

    allow heroku choose the port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import "dotenv/config.js";
 import urls from './src/routes/urls.js'
 
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 app.set("view engine", "ejs");
 app.set("views", path.join(dirname(), "./src/views"));
